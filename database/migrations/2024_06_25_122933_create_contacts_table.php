b14a0e54af6df1cbf2a12c0ae06a5d88f3ddf49a
Rachid-Ait-Rhoudane <rachid2aitrhoudane@gmail.com>
2024-06-25 13:38:59 +0100

    add Organisation and Contacts models and migrations

--- database/migrations/2024_06_25_122933_create_contacts_table.php
+++ database/migrations/2024_06_25_122933_create_contacts_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('contacts', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('role');
-            $table->rememberToken();
+            $table->string('city');
+            $table->string('phone');
+            $table->unsignedBigInteger('organisation_id');
+            $table->foreign('organisation_id')->references('id')->on('organisations');
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('contacts');
     }
 };
